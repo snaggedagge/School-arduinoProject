c6950dc8c9bef9acd009070b6a705579178cd25d
Dag Karlsson <Dag.karlsson@ha.ax>
2018-11-03 04:37:19 +0200

    Tried to synchronize buttons. Failed.

---
+++ cmake-build-debug/CMakeFiles/Skolprojekt.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Dag Karlsson/Desktop/Skolprojekt/CanBusCommon/CanMessageHandler.cpp" "C:/Users/Dag Karlsson/Desktop/Skolprojekt/cmake-build-debug/CMakeFiles/Skolprojekt.dir/CanBusCommon/CanMessageHandler.cpp.obj"
+  "C:/Users/Dag Karlsson/Desktop/Skolprojekt/CanBusCommon/CanUtility.cpp" "C:/Users/Dag Karlsson/Desktop/Skolprojekt/cmake-build-debug/CMakeFiles/Skolprojekt.dir/CanBusCommon/CanUtility.cpp.obj"
+  "C:/Users/Dag Karlsson/Desktop/Skolprojekt/Canbus/Canbus.cpp" "C:/Users/Dag Karlsson/Desktop/Skolprojekt/cmake-build-debug/CMakeFiles/Skolprojekt.dir/Canbus/Canbus.cpp.obj"
+  "C:/Users/Dag Karlsson/Desktop/Skolprojekt/Canbus/MsgParsing.cpp" "C:/Users/Dag Karlsson/Desktop/Skolprojekt/cmake-build-debug/CMakeFiles/Skolprojekt.dir/Canbus/MsgParsing.cpp.obj"
+  "C:/Users/Dag Karlsson/Desktop/Skolprojekt/Canbus/VecN2KMsg.cpp" "C:/Users/Dag Karlsson/Desktop/Skolprojekt/cmake-build-debug/CMakeFiles/Skolprojekt.dir/Canbus/VecN2KMsg.cpp.obj"
+  "C:/Users/Dag Karlsson/Desktop/Skolprojekt/Canbus/mcp2515.cpp" "C:/Users/Dag Karlsson/Desktop/Skolprojekt/cmake-build-debug/CMakeFiles/Skolprojekt.dir/Canbus/mcp2515.cpp.obj"
+  "C:/Users/Dag Karlsson/Desktop/Skolprojekt/ProjectLibrary/Light.cpp" "C:/Users/Dag Karlsson/Desktop/Skolprojekt/cmake-build-debug/CMakeFiles/Skolprojekt.dir/ProjectLibrary/Light.cpp.obj"
+  "C:/Users/Dag Karlsson/Desktop/Skolprojekt/ProjectLibrary/TempMsgHandler.cpp" "C:/Users/Dag Karlsson/Desktop/Skolprojekt/cmake-build-debug/CMakeFiles/Skolprojekt.dir/ProjectLibrary/TempMsgHandler.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Canbus"
+  "../CanBusCommon"
+  "../ProjectLibrary"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
